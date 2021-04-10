bf8e5713ea1a3b2844e5cf198fdafbb1a9f38759
HBharathi <81897745+HBharathi@users.noreply.github.com>
2021-04-10 19:13:37 +0530

    Create Class.py

---
+++ Class.py
@@ -0,0 +1,13 @@
+class Point:
+    def move(self):
+        print("draw a line")
+
+    def join(self):
+        print("join the points")
+
+point1 = Point()
+point1.x=10
+point1.y=20
+point1.join()
+point1.move()
+print(point1.x)
