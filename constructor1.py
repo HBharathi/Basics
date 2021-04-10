478042ff91a53ee382bc32d8200793a1ea1d23f8
HBharathi <81897745+HBharathi@users.noreply.github.com>
2021-04-10 20:32:25 +0530

    Create constructor1.py

---
+++ constructor1.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self,name): #constructor
+        self.name=name
+
+    def talk(self):
+        print(f" Hey {self.name}, please speak up!!!")
+
+p1 = Person("Annu")
+p1.talk()
+
+p2 = Person("Bharathi")
+p2.talk()
